754f0a24475dba1a22cb62443f16123f971f670c
Rick Evans <rick@ricks.app>
2020-12-13 08:37:04 -0700

    Add asset image and update pubspec file

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,7 @@ void main() {
         ),
         body: Center(
           child: Image(
-            image: NetworkImage(
-                'https://imagesvc.meredithcorp.io/v3/mm/image?q=85&c=sc&poi=face&w=1800&h=942&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F38%2F2014%2F11%2F12221859%2Foksanakuzmina7.jpg'),
+            image: AssetImage('images/crawl.jpg'),
           ),
         ),
       ),
